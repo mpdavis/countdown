4bbbd59b77d68b8c1829a1f942822bdeda9dae91
Michael Davis <mike.philip.davis@gmail.com>
2013-05-22 15:39:55 -0500

    Flask

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, render_template, redirect, url_for
+
+# Determining the project root.
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+# Creating the Flask app object
+app = Flask(__name__, static_folder=os.path.join(PROJECT_ROOT, 'static'), static_url_path='/static')
+
+
+app.route('/', methods=['GET'])
+def index():
+    context = {}
+    return render_template('index.html', **context)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
